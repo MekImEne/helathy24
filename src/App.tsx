49df71afb214cf0f8d1565feab0e1e84661053cf
MekImEne <fi_mekideche@esi.dz>
2023-06-26 15:25:01 +0100

    prepare components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import './App.css';
+import {
+  AboutUs,
+  Banner,
+  Contact,
+  FAQ,
+  Feedbacks,
+  FindDoctor,
+  Footer,
+  GetApp,
+  Location,
+  Navbar,
+  Offers,
+  SearchHospital,
+  Sections,
+  TeamBanner,
+  WhyUs,
+
+} from './app/components';
 
 function App() {
   return (
     <div>
-      Healthy 24+ here
+      <Navbar />
+      <Banner />
+      <Sections />
+      <AboutUs />
+      <WhyUs />
+      <GetApp />
+      <FindDoctor />
+      <Offers />
+      <SearchHospital />
+      <Feedbacks />
+      <FAQ />
+      <TeamBanner />
+      <Contact />
+      <Location />
+      <Footer />
     </div>
   );
 }
---
+++ src/app/components/index.tsx
@@ -0,0 +1,15 @@
+export { default as Navbar } from './Navbar';
+export { default as Banner } from './Banner';
+export { default as Sections } from './Sections';
+export { default as AboutUs } from './AboutUs';
+export { default as WhyUs } from './WhyUs';
+export { default as GetApp } from './GetApp';
+export { default as FindDoctor } from './FindDoctor';
+export { default as Offers } from './Offers';
+export { default as SearchHospital } from './SearchHospital';
+export { default as Feedbacks } from './Feedbacks';
+export { default as FAQ } from './FAQ';
+export { default as TeamBanner } from './TeamBanner';
+export { default as Contact } from './Contact';
+export { default as Location } from './Location';
+export { default as Footer } from './Footer';
