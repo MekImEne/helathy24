895ccf07b98d143b728599e550921e22b0fd3843
MekImEne <fi_mekideche@esi.dz>
2023-06-28 00:51:58 +0100

    Search hospital responsiveness

--- src/app/Sections/SearchHospital.tsx
+++ src/app/Sections/SearchHospital.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { careness, hospital, ratingy } from '../assets'
+import Button from '../components/UI/Button';
 
 export default function SearchHospital() {
 
@@ -8,18 +9,18 @@ export default function SearchHospital() {
     };
 
     return (
-        <div id='search-hospital' className="my-10 flex flex-wrap items-center">
-            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-5/12 lg:mt-0">
-                <div className="">
+        <div id='search-hospital' className="my-5 sm:my-10 flex flex-wrap items-center">
+            <div className="mx-auto mt-1 sm:mt-24 flex w-full justify-center px-0 sm:px-4 md:w-5/12 lg:mt-0">
+                <div>
                     <div className="relative">
                         <img
                             alt="CardImage"
                             src={hospital}
-                            className="sm:h-[31rem] h-full w-full"
+                            className="h-[19rem] sm:h-[31rem] w-full"
                         />
                     </div>
                     <div>
-                        <div className='absolute -mt-44 mx-80'>
+                        <div className='absolute -mt-32 mx-32 sm:-mt-44 sm:mx-80'>
                             <div className='bg-WHITE shadow-lg w-64 h-16 flex flex-row items-center content-center'>
                                 <img src={careness} alt='help' className='mt-2' />
                                 <div>
@@ -32,7 +33,7 @@ export default function SearchHospital() {
                 </div>
             </div>
 
-            <div className="mx-auto -mt-8 w-full px-12 md:w-6/12">
+            <div className="mx-auto sm:-mt-8 w-full px-12 md:w-6/12">
                 <p className="mb-8 font-medium text-3xl text-BLUE2">
                     Search the Nearest hospital  <br /> from you
                 </p>
@@ -40,12 +41,13 @@ export default function SearchHospital() {
                     Rumah sakit adalah bagian integral dari suatu organisasi sosial
                     dan kesehatan dengan fungsi menyediakan pelayanan paripurna (komprehensif).
                 </p>
-                <button
-                    onClick={handleSearch}
-                    className="rounded-none bg-ORANGE hover:bg-ORANGE-700 text-WHITE py-2 px-8">
-                    Search
-                </button>
 
+                <Button
+                    onClick={handleSearch}
+                    text='Search'
+                    bgColor='ORANGE'
+                    textColor='WHITE'
+                />
             </div>
 
         </div>
