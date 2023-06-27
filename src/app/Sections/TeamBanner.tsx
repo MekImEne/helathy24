531e46af1899dda7f20748b0eba0f5549170bd26
MekImEne <fi_mekideche@esi.dz>
2023-06-28 00:23:31 +0100

    Team Banner add shadow

--- src/app/Sections/TeamBanner.tsx
+++ src/app/Sections/TeamBanner.tsx
@@ -6,8 +6,7 @@ export default function TeamBanner() {
         <div id="team-banner" className='w-full bg-LIGHT_GRAY pt-10 sm:pt-24'>
             <div className=''>
                 <div className='py-0 sm:py-4 text-center'>
-                    <p className='text-3xl sm:text-4xl mx-10 sm:mx-96 font-semibold text-BLACK'>
-                        {/* TODO : add shadow to this text */}
+                    <p className='text-3xl sm:text-4xl mx-10 sm:mx-96 font-extrabold text-BLACK drop-shadow-lg shadow-BLACK'>
                         Our secret to great virtual care is great doctors
                     </p>
                     <p className='text-TEXT_GREY mx-10 sm:mx-72 opacity-70 py-7 mb-3'>
