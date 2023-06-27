913823044a5fe050a4b4c05165e701788eb0054b
MekImEne <fi_mekideche@esi.dz>
2023-06-28 00:18:11 +0100

    Team Banner responsiveness

--- src/app/Sections/TeamBanner.tsx
+++ src/app/Sections/TeamBanner.tsx
@@ -3,14 +3,14 @@ import { doctors } from '../assets'
 
 export default function TeamBanner() {
     return (
-        <div id="team-banner" className='w-full bg-LIGHT_GRAY pt-24'>
+        <div id="team-banner" className='w-full bg-LIGHT_GRAY pt-10 sm:pt-24'>
             <div className=''>
-                <div className='py-4 text-center'>
-                    <p className='text-4xl mx-96 font-semibold text-BLACK'>
+                <div className='py-0 sm:py-4 text-center'>
+                    <p className='text-3xl sm:text-4xl mx-10 sm:mx-96 font-semibold text-BLACK'>
                         {/* TODO : add shadow to this text */}
                         Our secret to great virtual care is great doctors
                     </p>
-                    <p className='text-TEXT_GREY mx-72 opacity-70 py-7 mb-3'>
+                    <p className='text-TEXT_GREY mx-10 sm:mx-72 opacity-70 py-7 mb-3'>
                         Board-certified | Top 5% of medical specialists in the world | +90% satisfaction rating
                     </p>
                 </div>
