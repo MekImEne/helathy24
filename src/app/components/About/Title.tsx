72e1abcc41c9f46d119513da0b09c259eeb643d4
MekImEne <fi_mekideche@esi.dz>
2023-06-28 04:06:31 +0100

    fix Title

--- src/app/components/About/Title.tsx
+++ src/app/components/About/Title.tsx
@@ -3,8 +3,8 @@ import { logo } from '../../assets';
 
 export default function Title({ text }: { text: string }) {
     return (
-        <div className="flex flex-row items-center mb-10">
-            <h2 className="text-4xl sm:text-5xl mt-6 mr-3 font-medium text-TEXT_GREY "> {text} </h2>
+        <div className="flex flex-col sm:flex-row items-center mb-10">
+            <h2 className="text-4xl sm:text-5xl mt-6 mr-8 sm:mr-3 font-medium text-TEXT_GREY "> {text} </h2>
             <img alt='healthy24+' src={logo} className='h-[5.5rem] sm:h-28 sm:w-auto' />
         </div>
     )
